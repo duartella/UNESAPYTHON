cb9dc48d31cbadf2364dd2b8a5cf4af373b713a5
rafaella <108313652+duartella@users.noreply.github.com>
2023-05-22 12:46:51 -0300

    I tried use "def" for the first time

--- Unesa Python.py
+++ Unesa Python.py
@@ -1,7 +1,4 @@
-# Página Inicial de Cadastro
-def Cadastro():
-   print("Cadastro de Usuário")
-   Nome = input("Nome")
-   Idade = input("Idade")
-   Senha = input("Senha")
+print(“Hello World”)
+
+
 
